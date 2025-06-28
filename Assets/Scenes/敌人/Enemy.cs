8957b7b84709d8a5178f671595a3689801eb37f7
Hbiris <hanzhiirisbian@gmail.com>
2025-06-28 17:36:53 +0800

    enemy

---
+++ Assets/Scenes/敌人/Enemy.cs
@@ -0,0 +1,43 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Pathfinding;
+//using System.Numerics;
+
+//public class Enemy : MonoBehaviour
+//{
+//    public Transform player;
+
+//    private Seeker seeker;
+//    private List<Vector3> pathPointList;
+//    private int currentIndex = 0;
+//    private float pathGenerationInterval = 0.5f;//0.5秒生成一次路径
+//    private float pathGenerateTimer = 0f;
+
+//    private void Awake()
+//    {
+//        seeker = GetComponent<Seeker>();
+//    }
+
+//    private void Update()
+//    {
+
+//    }
+
+//    private void AutoPath()
+//    {
+//        if (pathPointList == null || pathPointList.Count <= 0)
+//        {
+//            GeneratePath(player.position);
+//        }
+//    }
+//    //获取路径点
+//    private void GeneratePath(Vector3 target)
+//    {
+//        currentIndex = 0;
+//        seeker.StartPath(transform.position, target, Path =>
+//        {
+//            pathPointList = Path.vectorPath;
+//        });
+//    }
+//}
