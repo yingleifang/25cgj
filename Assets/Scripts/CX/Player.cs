d1dd1d5005fb2cf0d24940f1fd51ffd9f9e178ea
Kaleidoscopic <1263939210@qq.com>
2025-06-28 15:24:47 +0800

    San值UI
    UI跟踪主角Currentsan值属性，主角自身的Currentsan值随时间变化,相关逻辑在Player和SanityBar两个脚本中。

---
+++ Assets/Scripts/CX/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using QFramework;
+public class Player : MonoBehaviour
+{
+
+
+    public float PreSanity;//初始san值
+    public BindableProperty<float> CurrentSanity { get; } = new BindableProperty<float>();//当前san值
+    public float timer;
+    public float changeinterval = 1f;
+
+    void Start()
+    {
+        timer = 0;
+        CurrentSanity.Value = PreSanity;
+        //CurrenSanity.Value 每秒减一
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= changeinterval)
+        {
+            timer = 0;
+            CurrentSanity.Value -= 1.0f;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/CX/SanityBar.cs
@@ -0,0 +1,32 @@
+using QFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SanityBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject player;
+
+    void Start()
+    {
+        //注册对于player上的sanity的变化监听
+        player.GetComponent<Player>().CurrentSanity.Register(value =>
+        {
+            Debug.Log("当前sanity：" + value);
+            //更新Scrollbar的显示
+
+            GetComponent<Scrollbar>().size = player.GetComponent<Player>().CurrentSanity.Value / player.GetComponent<Player>().PreSanity;
+            Debug.Log("scrollbar value:" + GetComponent<Scrollbar>().size);
+        }
+            ).UnRegisterWhenGameObjectDestroyed(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
